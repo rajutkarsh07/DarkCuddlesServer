ea49fd4aab8808fc3c74df241a5b5b4b94fdd7d3
utkarsh_raj <rajutkarsh2505@gmail.com>
2024-04-06 23:35:38 +0530

    added database etc

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const userRoutes = require('./routes/user');
 
 const app = express();
 
+app.use(cookieParser());
+
 app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
